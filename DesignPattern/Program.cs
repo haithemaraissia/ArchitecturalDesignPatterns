a4f2350a158c85752f370a597fa72366fe5de952
haithem <haithemaraissia@yahoo.com>
2018-02-02 10:01:22 -0600

    Factory Pattern

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DesignPattern.DP.Creational.Factory;
+using DesignPattern.DP.Creational.Factory.Example.AirlineFactory.Creator.Concrete;
+using DesignPattern.DP.Creational.Factory.Example.AirlineFactory.Creator.Factory;
 
 namespace DesignPattern
 {
@@ -9,6 +12,26 @@ namespace DesignPattern
     {
         public static void Main(string[] args)
         {
+            FactoryPattern();
+        }
+
+        private static void FactoryPattern()
+        {
+            var airlines = new string[]
+            {
+                "delta",
+                "jetblue",
+                "united"
+            };
+
+            var factory = new ConcreteAirlineFactory();
+
+            foreach (var airline in airlines)
+            {
+                var airlineType = factory.GetAirline(airline);
+                Console.Write("Airline: {0} -- Price: {1}", airline, airlineType.GetPrice());
+                Console.ReadLine();
+            }
         }
     }
 }
